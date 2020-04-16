19230ed53b6e5b8870e04aa8404d28614fc03152
unknown <alexrich700@gmail.com>
2020-04-16 03:46:17 -0500

    Updates

---
+++ precache-manifest.40074f48d69ec4f01bd5b89428bc0cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09018eedfdc41775b8a08c5b1a3d246",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "da393592883e7fed037b",
+    "url": "/COVID-19-Tracker/static/css/main.41b62d49.chunk.css"
+  },
+  {
+    "revision": "0e515a9c20e967d8c3fe",
+    "url": "/COVID-19-Tracker/static/js/2.90657fdc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.90657fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da393592883e7fed037b",
+    "url": "/COVID-19-Tracker/static/js/main.d5062c23.chunk.js"
+  },
+  {
+    "revision": "7006992dc2429d41aeab",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.91276e9c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
